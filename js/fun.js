1fa09c84952b7f6c18375d6a4915b6a5cb3f1c78
Alex <you@example.com>
2016-05-02 17:09:31 -0400

    Getting there
    another iteration

--- js/fun.js
+++ js/fun.js
@@ -1,9 +1,11 @@
+function addShake(){
+        $(this).addClass("animated shake");
+}
 
+//Once the docuement is loaded do these things
 $(document).ready(function(){
 
-    $('.block').click(function(){
-        $(this).addClass("animated shake");
-    });
+    $('.block').click(addShake);
 
 
 
